d4881a35086ccd6a7f64f59426066105b48d1566
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2024-11-07 21:54:31 +0200

    workshop components

---
+++ 0.Workshop/workshop/src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [ThemesListComponent, PostsListComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
+})
+export class MainComponent {}
