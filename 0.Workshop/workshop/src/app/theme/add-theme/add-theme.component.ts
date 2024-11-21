d1d8acfcb9c4504a992536ae293c09f0262275fc
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2024-11-21 21:55:05 +0200

    workshop forms

--- 0.Workshop/workshop/src/app/theme/add-theme/add-theme.component.ts
+++ 0.Workshop/workshop/src/app/theme/add-theme/add-theme.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-theme',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-theme.component.html',
   styleUrl: './add-theme.component.css',
 })
 export class AddThemeComponent {
   constructor(private apiService: ApiService) {}
 
-  addTheme(event: Event, themeName: string, postText: string) {
-    event.preventDefault();
+  addTheme(form: NgForm) {
+    console.log(form);
 
-    this.apiService.createTheme(themeName, postText).subscribe((data) => {
-      console.log(data);
-    });
+    if (form.invalid) {
+      return;
+    }
+
+    console.log(form.value);
+
+    // this.apiService.createTheme(themeName, postText).subscribe((data) => {
+    //   console.log(data);
+    // });
   }
 }
