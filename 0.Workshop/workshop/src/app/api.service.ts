3c7fd3fd583c2c60b843c373a65701d05a0878a0
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2024-11-27 21:38:03 +0200

    workshop pipes and interceptors

--- 0.Workshop/workshop/src/app/api.service.ts
+++ 0.Workshop/workshop/src/app/api.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment.development';
 import { Post } from './types/post';
 import { Theme } from './types/theme';
 
@@ -11,8 +10,7 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getPosts(limit?: number) {
-    const { apiUrl } = environment;
-    let url = `${apiUrl}/posts`;
+    let url = `/api/posts`;
     if (limit) {
       url += `?limit=${limit}`;
     }
@@ -21,18 +19,35 @@ export class ApiService {
   }
 
   getThemes() {
-    const { apiUrl } = environment;
-    return this.http.get<Theme[]>(`${apiUrl}/themes`);
+    return this.http.get<Theme[]>(`/api/themes`);
   }
 
   getSingleTheme(id: string) {
-    const { apiUrl } = environment;
-    return this.http.get<Theme>(`${apiUrl}/themes/${id}`);
+    return this.http.get<Theme>(`/api/themes/${id}`);
   }
 
   createTheme(themeName: string, postText: string) {
-    const { apiUrl } = environment;
     const payload = { themeName, postText };
-    return this.http.post<Theme>(`${apiUrl}/themes`, payload);
+    return this.http.post<Theme>(`/api/themes`, payload);
+  }
+
+  // CRUD operations
+  // update -> http.put
+  updateTheme(themeId: string, themeName: string, postText: string) {
+    const payload = { themeName, postText };
+    return this.http.put<Theme>(`/api/themes/${themeId}`, payload);
+  }
+
+  updatePost(themeId: string, postId: string) {
+    const payload = {};
+    return this.http.put<Theme>(
+      `/api/themes/${themeId}/posts/${postId}`,
+      payload
+    );
+  }
+
+  // delete -> http.delete theme ID
+  deletePost(themeId: string, postId: string) {
+    return this.http.delete(`/api/themes/${themeId}/posts/${postId}`);
   }
 }
--- 0.Workshop/workshop/src/app/error/error.component.ts
+++ 0.Workshop/workshop/src/app/error/error.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-error',
+  selector: 'app-page-not-found',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './error.component.html',
   styleUrl: './error.component.css',
 })
-export class ErrorComponent {}
+export class PageNotFoundComponent {}
